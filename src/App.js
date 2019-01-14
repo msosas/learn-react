e55ebdab113492bf886860f089d8bea8bed67ebd
Matias Sosa <matiassosas@gmail.com>
2019-01-14 14:39:56 -0300

    Exercise

--- src/App.js
+++ src/App.js
@@ -1,39 +1,15 @@
 import React, { Component } from 'react';
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
-import Footer from './Footer'
+import Product from './Product'
+import products from './dbProducts'
 
-// Exercise 01
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Exercise 01</h1>
-//         <ul>
-//           <li>item01</li>
-//           <li>item02</li>
-//           <li>item03</li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
 
-// Exercise 02
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <h1>Matías Sosa</h1>
-        <p>Computer Engineer | DevOps Engineer</p>
-        <div>
-          <ol>
-            <li>Cancún</li>
-            <li>Amsteram</li>
-            <li>Tafí del Valle</li>
-          </ol>
-        </div>
-        <Footer />
+        <Product />
       </div>
     )
   }
