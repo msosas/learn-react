22e6bf8b512ea2b80f7291c686bbe3562b186ae7
Matias Sosa <matiassosas@gmail.com>
2019-01-14 14:27:54 -0300

    Delete comments

--- src/App.js
+++ src/App.js
@@ -3,29 +3,6 @@ import logo, { ReactComponent } from './logo.svg';
 import './App.css';
 import Footer from './Footer'
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
-
 // Exercise 01
 // class App extends React.Component {
 //   render() {
