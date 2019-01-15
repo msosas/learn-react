182bf7014ad8e4b94571fd6e40debe1189d6a0fb
Matias Sosa <matiassosas@gmail.com>
2019-01-15 20:57:32 -0300

    Exercise 32

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
-import React, { Component } from 'react';
-import logo, { ReactComponent } from './logo.svg';
-import './App.css';
-import Footer from './Footer'
+import React from "react"
+import './style.css'
 
-// Exercise 01
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Exercise 01</h1>
-//         <ul>
-//           <li>item01</li>
-//           <li>item02</li>
-//           <li>item03</li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
-
-// Exercise 02
-class App extends Component {
+class App extends React.Component {
+  constructor () {
+    super()
+    this.state = {
+      counter: 0
+    }
+  }
   render () {
     return (
-      <div className="App">
-        <h1>Matías Sosa</h1>
-        <p>Computer Engineer | DevOps Engineer</p>
-        <div>
-          <ol>
-            <li>Cancún</li>
-            <li>Amsteram</li>
-            <li>Tafí del Valle</li>
-          </ol>
-        </div>
-        <Footer />
+      <div>
+        <h2>{ this.state.counter }</h2>
+        <button onClick= { () => this.setState({counter: this.state.counter +1})}>Change</button>
       </div>
     )
   }
-}
+}  
 
-// Ejercicio 03
-export default App;
+export default App
