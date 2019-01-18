973b792ea5b9230b2d3976e89c8aa8111ba5ca01
Matias Sosa <matiassosas@gmail.com>
2019-01-18 10:18:26 -0300

    Create Spinner component

--- src/App.js
+++ src/App.js
@@ -1,43 +1,30 @@
 import React, { Component } from 'react';
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
-import Footer from './Footer'
+import Spinner from './Spinner'
 
-// Exercise 01
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>Exercise 01</h1>
-//         <ul>
-//           <li>item01</li>
-//           <li>item02</li>
-//           <li>item03</li>
-//         </ul>
-//       </div>
-//     );
-//   }
-// }
 
-// Exercise 02
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      isLoading: true
+    }
+  }
+  componentDidMount () {
+    setInterval(() => {
+      this.setState({
+        isLoading: false
+      })
+    }, 2000)
+  }
   render () {
     return (
       <div className="App">
-        <h1>Matías Sosa</h1>
-        <p>Computer Engineer | DevOps Engineer</p>
-        <div>
-          <ol>
-            <li>Cancún</li>
-            <li>Amsteram</li>
-            <li>Tafí del Valle</li>
-          </ol>
-        </div>
-        <Footer />
+        { this.state.isLoading ? <Spinner /> : <h1>Loaded</h1>}
       </div>
     )
   }
 }
 
-// Ejercicio 03
 export default App;
