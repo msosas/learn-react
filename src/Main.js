4f239438b636f5dcc3f0214d77972032ab641980
Matias Sosa <matiassosas@gmail.com>
2019-01-24 13:41:28 -0300

    Update master branch

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Main extends React.Component {
+    render () {
+        return (
+            <div className="Main">
+                <main>Main</main>
+            </div>
+        )
+    }
+}
+
+export default Main;
